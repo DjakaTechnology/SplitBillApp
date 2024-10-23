5314642f830407c1efc2b9a8ed1907de339b7063
DjakaTechnology <djakajaya898@gmail.com>
2024-10-23 19:37:38 +0700

    Added bottom

--- composeApp/src/commonMain/kotlin/id/djaka/splitbillapp/input/item/InputItemsScreen.kt
+++ composeApp/src/commonMain/kotlin/id/djaka/splitbillapp/input/item/InputItemsScreen.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
@@ -124,6 +126,10 @@ fun InputItemsWidget(
             Column(
                 verticalArrangement = Arrangement.spacedBy(Spacing.s),
                 modifier = Modifier.padding(horizontal = Spacing.m)
+                    .padding(bottom = Spacing.xl)
+                    .verticalScroll(
+                        rememberScrollState()
+                    )
             ) {
                 Column(verticalArrangement = Arrangement.spacedBy(Spacing.s)) {
                     itemList.fastForEachIndexed { index, it ->
